c6b04987c663009cd95b1f1f768f448a314ace81
Piotr Pyśk <defcon1994@gmail.com>
2019-02-04 23:39:42 +0100

    Upgrade to Angular 7

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -1,13 +1,14 @@
-import {NgModule} from '@angular/core';
-import {AllProjectsComponent} from './all-projects/all-projects.component';
-import {ProjectDetailsComponent} from './project-details/project-details.component';
-import {AddProjectComponent} from './add-project/add-project.component';
-import {ProjectSummaryComponent} from './project-summary/project-summary.component';
-import {ProjectsRoutingModule} from "./projects-routing.module";
-import {ProjectsService} from "./projects.service";
-import {TasksModule} from "../tasks/tasks.module";
-import {SharedModule} from "../shared/shared.module";
-import {SearchModule} from "../search/search.module";
+import { NgModule } from '@angular/core';
+import { AllProjectsComponent } from './all-projects/all-projects.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { AddProjectComponent } from './add-project/add-project.component';
+import { ProjectSummaryComponent } from './project-summary/project-summary.component';
+import { ProjectsRoutingModule } from './projects-routing.module';
+import { ProjectsService } from './projects.service';
+import { TasksModule } from '../tasks/tasks.module';
+import { SharedModule } from '../shared/shared.module';
+import { SearchModule } from '../search/search.module';
+import { DatePipe } from '@angular/common';
 
 @NgModule({
   imports: [
@@ -23,7 +24,8 @@ import {SearchModule} from "../search/search.module";
     ProjectSummaryComponent
   ],
   providers: [
-    ProjectsService
+    ProjectsService,
+    DatePipe
   ]
 })
 export class ProjectsModule {
